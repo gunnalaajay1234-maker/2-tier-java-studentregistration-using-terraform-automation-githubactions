24114f9644697c36cec1af63ea4c7687c8df1315
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-25 17:34:12 +0530

    s3 bucket created

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "ajay-terraform-state-bucket"
+    bucket  = "ajay-terraform-state-bucket-123"
     key     = "2tier/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
