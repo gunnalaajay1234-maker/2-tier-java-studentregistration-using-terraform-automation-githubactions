a31b03a747266252a94ad049f5709568aaa5fa83
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-25 15:32:09 +0530

    Initial commit

--- user_data.sh
+++ user_data.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
+
 sudo apt update -y
 
-# INSTALL JAVA + MAVEN
+# Install Java + Maven
 sudo apt install openjdk-17-jdk -y
 sudo apt install maven -y
+sudo apt install git -y
 
+# Clone your repo
+cd /home/ubuntu
+git clone https://github.com/gunnalaajay1234-maker/2-tier-java-studentregistration-using-terraform-automation-githubactions.git
+
+# Build app
+cd https://github.com/gunnalaajay1234-maker/2-tier-java-studentregistration-using-terraform-automation-githubactions.git/app
+mvn clean package
+
+# Run app
+java -jar target/*.jar > app.log 2>&1 &
